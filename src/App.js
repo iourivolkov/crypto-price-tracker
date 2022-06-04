5022a1d61aca825d935b36b28551672ca6bb002b
Iouri Volkov <volk.iouri@gmail.com>
2022-06-04 09:30:48 -0400

    Install axios, create fetchQueryData function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navigation from "./components/Navigation";
 import PriceHero from "./components/PriceHero";
 import Top10List from "./components/Top10List";
 import { useState } from "react";
+import { axios } from "axios";
 
 function App() {
   const title = "Trackr";
   const subtitle = "Track crypto and NFTs from one spot";
   const titleFooter = "Top 10 Marketcap";
   const [inputText, setInputText] = useState("");
+  const [indivPriceData, setIndivPriceData] = useState();
 
   const handleInputText = (e) => {
     setInputText(e.target.value);
