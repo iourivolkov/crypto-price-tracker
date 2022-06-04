adaadad749485c948163d8b123357f850cf6f606
Iouri Volkov <volk.iouri@gmail.com>
2022-06-03 22:31:22 -0400

    Complete Price data component

--- src/components/PriceDataCard.jsx
+++ src/components/PriceDataCard.jsx
@@ -2,20 +2,20 @@ const PriceDataCard = () => {
   return (
     <div className="price-data-card">
       <header className="price-data-header">
-        <span className="token-name">Bitcoin</span>
-        <span className="token-symbol">BTC</span>
+        <div className="token-name">Bitcoin</div>
+        <div className="token-symbol">BTC</div>
       </header>
       <img className="price-chart" src="" alt="price-chart" />
-      <section>
-        <span className="token-price">$39,938.23</span>
-        <span className="price-change">+1.02%</span>
+      <section className="token-price-change">
+        <div className="token-price">$39,938.23</div>
+        <div className="price-change">+1.02%</div>
       </section>
-      <section>
+      <section className="mc-dv">
         <div className="market-cap">
-          <b>Market Cap</b> $1,989,383,438.43
+          <b>Market Cap:</b> $1,989,383,438.43
         </div>
         <div className="daily-volume">
-          <b>24hr Volume</b> $987,438,438,28
+          <b>24hr Volume:</b> $987,438,438,28
         </div>
       </section>
     </div>
