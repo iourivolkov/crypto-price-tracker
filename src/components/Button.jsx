6056827521546d72f779fa0aad1a22d107aa2a1e
Iouri Volkov <volk.iouri@gmail.com>
2022-06-12 12:39:08 -0400

    Change landing page background, change button color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = () => {
   return (
     <>
-      <button className="connect-wallet-btn">Connect Wallet</button>
+      <button className="connect-wallet-btn">Enter</button>
     </>
   );
 };
