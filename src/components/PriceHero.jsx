939fdcb758c2dd92cf3037d32cc5f448cde7a8ba
Iouri Volkov <volk.iouri@gmail.com>
2022-06-20 11:59:59 -0400

    Add placeholder image conditionally if PriceData has not been returned

--- src/components/PriceHero.jsx
+++ src/components/PriceHero.jsx
@@ -17,6 +17,12 @@ const PriceHero = ({
         inputText={inputText}
         getPrice={getPrice}
       />
+      {!displayPriceData && (
+        <img
+          src="https://images.unsplash.com/photo-1638913970961-1946e5ee65c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3432&q=80"
+          className="placeholder-img"
+        />
+      )}
       {displayPriceData && (
         <PriceDataCard
           tokenData={priceQueryData}
